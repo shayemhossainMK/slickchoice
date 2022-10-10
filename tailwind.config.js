9af600add0e1cf9a7d77c98571864151a6a16e96
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-10-10 10:30:36 +0600

    new code added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#48BF91",
+          secondary: "#033F63",
+          accent: "#FFECC6",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
